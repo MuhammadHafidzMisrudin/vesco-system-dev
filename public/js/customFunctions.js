c47120d6cc554c4501faba79482a7d5ff2013be2
Hafidz <halfpheez92@gmail.com>
2019-11-18 23:35:41 +0800

    initialised wowjs and added wow class attributes for animations.

--- public/js/customFunctions.js
+++ public/js/customFunctions.js
@@ -3,3 +3,9 @@
            SERVICES AND SOLUTIONS
 =============================================
 */
+
+// make functions available after page is empty.
+$(() => {
+    // animate on scrolling.
+    new WOW().init();
+});
