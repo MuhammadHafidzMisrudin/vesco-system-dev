7640e094e0ed86eea429f28f474fabe9e384eb8a
Hafidz <halfpheez92@gmail.com>
2019-12-06 00:50:23 +0800

    initialised popup gallery effect function for work.

--- public/js/customFunctions.js
+++ public/js/customFunctions.js
@@ -3,9 +3,25 @@
            SERVICES AND SOLUTIONS
 =============================================
 */
-
 // make functions available after page is empty.
 $(() => {
     // animate on scrolling.
     new WOW().init();
+});
+
+/* 
+=============================================
+           PORTFOLIOS & WORK
+=============================================
+*/
+// make functions available after page is empty.
+$(() => {
+    // popup initialization. 
+    $('#work').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open.
+        type: 'image',
+        gallery: { 
+            enabled: true 
+        }
+    });
 });
